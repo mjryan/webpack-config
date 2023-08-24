a09a2f86b6ed85b5ac13a4ec4e2e64b9178f0452
Michael Ryan <mjryan@hotmail.com>
2023-08-23 20:20:29 -0500

    add css, sass, postcss, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -11,6 +13,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(s[ac]|c)css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader", 
+                    "postcss-loader", 
+                    "sass-loader"
+                ],
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
@@ -20,10 +31,13 @@ module.exports = {
         ],
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool: "source-map",
     devServer: {
         static: {
             directory: path.join(__dirname, './dist'), // Replace 'public' with your actual directory
           },
+          hot: true,
       },
   };
