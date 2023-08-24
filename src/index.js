ab5c00be1f6faed8ee30f30fcf6f616f7ceeff8c
Michael Ryan <mjryan@hotmail.com>
2023-08-24 14:42:07 -0500

    add React 18 support by using createRoot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {render} from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
